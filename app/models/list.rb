074d16fadb44f204d1250343eba8591e861d2dfb
GLeonhardt <guileonhardt2@gmail.com>
2019-06-18 00:26:32 -0300

    Some validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
+    validates_presence_of :name
+    validates_uniqueness_of :name
     has_many :products, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  validates_presence_of :name, :price
   belongs_to :list
 end
